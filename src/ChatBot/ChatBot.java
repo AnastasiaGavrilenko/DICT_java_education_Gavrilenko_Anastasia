5eb7a203556c2159c013574c75f603e9b2af55be
AnastasiaGavrilenko <a.a.havrylenko@student.khai.edu>
2023-10-18 17:00:34 +0300

    ChatBot 2nd stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ChatBot {
     public static void main(String[] args) {
         // Отримуємо поточний рік
@@ -6,5 +8,15 @@ public class ChatBot {
         // Виводимо привітання та інформацію про бота
         System.out.println("Hello! My name is MyBot.");
         System.out.println("I was created in " + currentYear + ".");
+
+        // Створюємо об'єкт Scanner для зчитування імені користувача
+        Scanner scanner = new Scanner(System.in);
+
+        // Запитуємо у користувача його ім'я
+        System.out.println("Please, remind me your name.");
+        String userName = scanner.nextLine();
+
+        // Виводимо персональне привітання з іменем користувача
+        System.out.println("What a great name you have, MyName " + userName + "!");
     }
 }
