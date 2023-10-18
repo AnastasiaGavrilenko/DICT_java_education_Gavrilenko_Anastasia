654213b83fc3a4e11ca875f77ec1b55b011bfd96
AnastasiaGavrilenko <a.a.havrylenko@student.khai.edu>
2023-10-18 17:07:50 +0300

    ChatBot 3rd stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -14,9 +14,24 @@ public class ChatBot {
 
         // Запитуємо у користувача його ім'я
         System.out.println("Please, remind me your name.");
-        String userName = scanner.nextLine();
+        String MyName = scanner.nextLine();
 
         // Виводимо персональне привітання з іменем користувача
-        System.out.println("What a great name you have, MyName " + userName + "!");
+        System.out.println("What a great name you have, MyName " + MyName + "!");
+
+        // Спробуємо вгадати вік користувача
+        System.out.println("Let me guess your age.");
+        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+
+        // Зчитуємо залишки від ділення на 3, 5 і 7
+        int remainder3 = Integer.parseInt(scanner.nextLine());
+        int remainder5 = Integer.parseInt(scanner.nextLine());
+        int remainder7 = Integer.parseInt(scanner.nextLine());
+
+        // Обчислюємо вік за формулою
+        int userAge = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
+
+        // Виводимо вгаданий вік
+        System.out.println("Your age is " + userAge + "; that's a good time to start programming!");
     }
 }
