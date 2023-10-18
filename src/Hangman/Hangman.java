1e562883fc967fe7b2f5bde33f2565d182e03652
AnastasiaGavrilenko <a.a.havrylenko@student.khai.edu>
2023-10-18 18:32:41 +0300

    Додано анонс гри

---
+++ src/Hangman/Hangman.java
@@ -0,0 +1,10 @@
+package Hangman; 
+public class Hangman {
+    public static void main(String[] args) {
+        // Вивести анонс
+        System.out.println("HANGMAN");
+        System.out.println("The game will be available soon.");
+
+        // Тут ви можете додати логіку для головного меню та інші етапи гри.
+    }
+}
