c10ac3a774fe25a85baa118b71820357bcca715a
AnastasiaGavrilenko <a.a.havrylenko@student.khai.edu>
2023-10-31 11:35:23 +0200

    Крок 3: Перевірка к-сті інгредієнтів

--- CoffeeMachine/CoffeeMachine.java
+++ CoffeeMachine/CoffeeMachine.java
@@ -5,16 +5,31 @@ public class CoffeeMachine {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("How many cups of coffee would you like to make?");
-        int cups = scanner.nextInt();
+        System.out.println("Write how many ml of water the coffee machine has:");
+        int waterAvailable = scanner.nextInt();
 
-        int waterNeeded = cups * 200;      // 200 ml of water per cup
-        int milkNeeded = cups * 50;        // 50 ml of milk per cup
-        int coffeeBeansNeeded = cups * 15; // 15 g of coffee beans per cup
+        System.out.println("Write how many ml of milk the coffee machine has:");
+        int milkAvailable = scanner.nextInt();
 
-        System.out.println("To make " + cups + " cups of coffee, you will need:");
-        System.out.println(waterNeeded + " ml of water");
-        System.out.println(milkNeeded + " ml of milk");
-        System.out.println(coffeeBeansNeeded + " g of coffee beans");
+        System.out.println("Write how many grams of coffee beans the coffee machine has:");
+        int coffeeBeansAvailable = scanner.nextInt();
+
+        System.out.println("Write how many cups of coffee you will need:");
+        int cupsNeeded = scanner.nextInt();
+
+        int cupsByWater = waterAvailable / 200;
+        int cupsByMilk = milkAvailable / 50;
+        int cupsByCoffeeBeans = coffeeBeansAvailable / 15;
+
+        int possibleCups = Math.min(cupsByWater, Math.min(cupsByMilk, cupsByCoffeeBeans));
+
+        if (cupsNeeded == possibleCups) {
+            System.out.println("Yes, I can make that amount of coffee");
+        } else if (cupsNeeded < possibleCups) {
+            int extraCups = possibleCups - cupsNeeded;
+            System.out.println("Yes, I can make that amount of coffee (and even " + extraCups + " more than that)");
+        } else {
+            System.out.println("No, I can make only " + possibleCups + " cups of coffee");
+        }
     }
 }
