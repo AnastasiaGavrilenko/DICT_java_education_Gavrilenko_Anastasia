5c0e2a496b6596bf1456988bdca40b0350dec4d4
AnastasiaGavrilenko <a.a.havrylenko@student.khai.edu>
2023-11-19 17:24:47 +0200

    Перший етап

---
+++ TicTacToe/TicTacToe.java
@@ -0,0 +1,12 @@
+public class TicTacToe {
+
+    public static void main(String[] args) {
+        printGameBoard();
+    }
+
+    private static void printGameBoard() {
+        System.out.println("X O X");
+        System.out.println("O X O");
+        System.out.println("X X O");
+    }
+}
