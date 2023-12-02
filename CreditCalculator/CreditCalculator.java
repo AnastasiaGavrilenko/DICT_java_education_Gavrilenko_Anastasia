d844a3c9f5b19deb59b5c3b884e55a8d55c3e4da
AnastasiaGavrilenko <a.a.havrylenko@student.khai.edu>
2023-12-02 15:31:16 +0200

    Другий етап

--- CreditCalculator/CreditCalculator.java
+++ CreditCalculator/CreditCalculator.java
@@ -1,16 +1,33 @@
+import java.util.Scanner;
+
 public class CreditCalculator {
     public static void main(String[] args) {
-        double loanPrincipal = 1000.0;
-        double monthlyPayment = 250.0;
-        int month = 1;
-        while (loanPrincipal > 0) {
-            System.out.println("Month " + month + ": repaid " + monthlyPayment);
-            loanPrincipal -= monthlyPayment;
+        Scanner scanner = new Scanner(System.in);
 
-            if (loanPrincipal <= 0) {
-                System.out.println("The loan has been repaid!");
+        // Запит на сумму кредиту
+        System.out.println("Enter the loan principal:");
+        double loanPrincipal = scanner.nextDouble();
+
+        // Запит на який параметр користувач хоче розрахувати
+        System.out.println("What do you want to calculate?");
+        System.out.println("Type \"m\" – for number of monthly payments,");
+        System.out.println("Type \"p\" – for the monthly payment:");
+        String calculationType = scanner.next();
+
+        if (calculationType.equals("m")) {
+            System.out.println("Enter the monthly payment:");
+            double monthlyPayment = scanner.nextDouble();
+            int numberOfMonths = (int) Math.ceil(loanPrincipal / monthlyPayment);
+            System.out.println("It will take " + numberOfMonths + " months to repay the loan");
+        } else if (calculationType.equals("p")) {
+            System.out.println("Enter the number of months:");
+            int numberOfMonths = scanner.nextInt();
+            double monthlyPayment = loanPrincipal / numberOfMonths;
+            double lastPayment = loanPrincipal - (numberOfMonths - 1) * monthlyPayment;
+            System.out.println("Your monthly payment = " + (int) Math.ceil(monthlyPayment));
+            if (lastPayment != monthlyPayment) {
+                System.out.println("and the last payment = " + (int) Math.ceil(lastPayment));
             }
-            month++;
         }
     }
 }
