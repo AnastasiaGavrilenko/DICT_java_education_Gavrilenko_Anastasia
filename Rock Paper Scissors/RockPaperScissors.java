b567566db82ad469e1f1f2941e4f5ef132a820e1
AnastasiaGavrilenko <a.a.havrylenko@student.khai.edu>
2023-12-15 16:34:06 +0200

    Четвертий етап

--- Rock Paper Scissors/RockPaperScissors.java
+++ Rock Paper Scissors/RockPaperScissors.java
@@ -1,9 +1,20 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Random;
 import java.util.Scanner;
 
 public class RockPaperScissors {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your name: > ");
+        String userName = scanner.nextLine();
+
+        int userScore = readUserScore(userName);
+
+        System.out.println("Hello, " + userName);
 
         while (true) {
             System.out.print("> ");
@@ -12,24 +23,33 @@ public class RockPaperScissors {
             if (userInput.equals("!exit")) {
                 System.out.println("Bye!");
                 break;
+            } else if (userInput.equals("!rating")) {
+                System.out.println("Your rating: " + userScore);
             } else if (userInput.equals("rock") || userInput.equals("paper") || userInput.equals("scissors")) {
-                playGame(userInput);
+                playGame(userInput, userName, userScore);
             } else {
                 System.out.println("Invalid input");
             }
         }
     }
-    private static void playGame(String userChoice) {
+
+    private static void playGame(String userChoice, String userName, int userScore) {
         String computerChoice = getRandomChoice();
         String result = getResult(userChoice, computerChoice);
+        int roundScore = calculateRoundScore(result);
+        int newScore = userScore + roundScore;
+        writeUserScore(userName, newScore);
+
         System.out.println(result);
     }
+
     private static String getRandomChoice() {
         String[] choices = {"rock", "paper", "scissors"};
         Random random = new Random();
         int index = random.nextInt(choices.length);
         return choices[index];
     }
+
     private static String getResult(String userChoice, String computerChoice) {
         if (userChoice.equals(computerChoice)) {
             return "There is a draw (" + computerChoice + ")";
@@ -43,5 +63,37 @@ public class RockPaperScissors {
             return "Sorry, but the computer chose " + computerChoice;
         }
     }
-}
 
+    private static int calculateRoundScore(String result) {
+        if (result.contains("draw")) {
+            return 50;
+        } else if (result.contains("Well done")) {
+            return 100;
+        } else {
+            return 0;
+        }
+    }
+
+    private static int readUserScore(String userName) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("out/rating.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(" ");
+                if (parts.length == 2 && parts[0].equals(userName)) {
+                    return Integer.parseInt(parts[1]);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    private static void writeUserScore(String userName, int userScore) {
+        try (FileWriter writer = new FileWriter("rating.txt", true)) {
+            writer.write(userName + " " + userScore + System.lineSeparator());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
