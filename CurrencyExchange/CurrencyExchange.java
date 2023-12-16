7975d32542a7a4fb3f16cb6069b7b79aa7b2a122
AnastasiaGavrilenko <a.a.havrylenko@student.khai.edu>
2023-12-16 14:28:39 +0200

    Третій етап

--- CurrencyExchange/CurrencyExchange.java
+++ CurrencyExchange/CurrencyExchange.java
@@ -1,48 +1,85 @@
-import java.text.DecimalFormat;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import org.json.JSONObject;
 import java.util.Scanner;
 
+
 public class CurrencyExchange {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        DecimalFormat df = new DecimalFormat("#.##"); // Форматування для округлення до двох знаків після коми
 
-        // Курси обміну для різних валют
-        Map<String, Double> exchangeRates = new HashMap<>();
-        exchangeRates.put("ARS", 0.82);
-        exchangeRates.put("HNL", 0.17);
-        exchangeRates.put("AUD", 1.9622);
-        exchangeRates.put("MAD", 0.208);
+        // Зчитування коду валюти від користувача
+        System.out.print("Please, enter the currency code (e.g., USD): ");
+        String baseCurrency = scanner.next().toUpperCase();
 
-        // Зчитування кількості валюти
-        double mycoins = 0.0;
-        while (true) {
-            try {
-                System.out.print("Please, enter the number of mycoins you have: ");
-                while (!scanner.hasNextDouble()) {
-                    System.out.println("Invalid input. Please enter a valid numeric value.");
-                    System.out.print("Please, enter the number of mycoins you have: ");
-                    scanner.next(); // прочитати невірне значення
-                }
-                mycoins = scanner.nextDouble();
-                break; // вихід з циклу, якщо вдало зчитано число
-            } catch (Exception e) {
-                System.out.println("Invalid input. Please enter a valid numeric value.");
-                scanner.next(); // прочитати невірне значення
-            }
-        }
+        // Зчитування кількості валюти від користувача
+        System.out.print("Please, enter the amount of " + baseCurrency + ": ");
+        double amount = scanner.nextDouble();
 
-        // Конвертація та виведення результатів
-        for (Map.Entry<String, Double> entry : exchangeRates.entrySet()) {
-            String currency = entry.getKey();
-            double rate = entry.getValue();
-            double convertedAmount = mycoins * rate;
-            System.out.println("I will get " + df.format(convertedAmount) + " " + currency +
-                    " from the sale of " + mycoins + " mycoins.");
-        }
+        // Виклик методу для отримання та виведення курсів обміну
+        getAndDisplayExchangeRates(baseCurrency, amount);
 
         // Закриття Scanner
         scanner.close();
+    }
+
+    private static void getAndDisplayExchangeRates(String baseCurrency, double amount) {
+        // Масив цільових валют
+        String[] targetCurrencies = {"USD", "EUR", "GBP", "JPY", "AUD"};
+
+        // Виведення заголовку результатів
+        System.out.println("Exchange rates for " + amount + " " + baseCurrency + ":");
+        System.out.println("-------------------------");
+
+        // Виведення курсів обміну для кожної цільової валюти
+        for (String targetCurrency : targetCurrencies) {
+            getExchangeRateAndPrint(baseCurrency, targetCurrency, amount);
+        }
+    }
+
+    private static void getExchangeRateAndPrint(String baseCurrency, String targetCurrency, double amount) {
+        try {
+            // Створення URL для запиту
+            URL url = new URL("https://api.exchangerate-api.com/v4/latest/" + baseCurrency);
+
+            // Отримання об'єкта HttpURLConnection
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            // Перевірка коду відповіді
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                // Читання відповіді
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                // Обробка JSON-відповіді
+                JSONObject jsonResponse = new JSONObject(response.toString());
+                JSONObject rates = jsonResponse.getJSONObject("rates");
+                double exchangeRate = rates.getDouble(targetCurrency);
+
+                // Обчислення суми у цільовій валюті
+                double targetAmount = amount * exchangeRate;
+
+                // Виведення результату
+                System.out.println(amount + " " + baseCurrency + " is equal to " + targetAmount + " " + targetCurrency);
+            } else {
+                System.out.println("Error fetching exchange rate. HTTP response code: " + responseCode);
+            }
+
+            // Закриття з'єднання
+            connection.disconnect();
+        } catch (Exception e) {
+            System.out.println("Error fetching exchange rate: " + e.getMessage());
+        }
     }
 }
